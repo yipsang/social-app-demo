c16994dc476cdf3c1575d14b92a676db701811dd
Yip Sang Leung <thirtythousandollar@gmail.com>
2019-02-27 21:54:02 +0800

    Update AppNavigator

--- src/components/AppNavigator.tsx
+++ src/components/AppNavigator.tsx
@@ -1,21 +1,73 @@
-import { createAppContainer, createStackNavigator } from "react-navigation";
+import {
+  createAppContainer,
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 import { HomeScreen, DetailsScreen, CounterScreen } from "./Screen";
+import PostScreen from "./PostScreen/PostScreen";
+import { withTabBar, TabID } from "./TabBar";
 
-export default createAppContainer(
-  createStackNavigator(
-    {
-      Home: {
-        screen: HomeScreen
-      },
-      Details: {
-        screen: DetailsScreen
-      },
-      Counter: {
-        screen: CounterScreen
-      }
-    },
-    {
-      initialRouteName: "Home"
+import * as Color from "../styles/colors";
+
+export enum RouteName {
+  PostTab = "PostTab",
+  AlbumTab = "AlbumTab"
+}
+
+const appNavigationOptions = {
+  headerTintColor: "white",
+  headerStyle: {
+    borderBottomWidth: 0,
+    borderWidth: 0,
+    backgroundColor: Color.mineShaft
+  }
+};
+
+const PostStackNavigator = createStackNavigator(
+  {
+    Posts: {
+      screen: withTabBar(PostScreen, TabID.Post),
+      navigationOptions: appNavigationOptions
     }
-  )
+  },
+  {
+    initialRouteName: "Posts",
+    navigationOptions: {
+      tabBarVisible: false
+    }
+  }
 );
+
+const AlbumStackNavigator = createStackNavigator(
+  {
+    Albums: {
+      screen: withTabBar(HomeScreen, TabID.Album),
+      navigationOptions: appNavigationOptions
+    },
+    Details: {
+      screen: DetailsScreen
+    },
+    Counter: {
+      screen: CounterScreen
+    }
+  },
+  {
+    initialRouteName: "Albums",
+    navigationOptions: {
+      tabBarVisible: false
+    }
+  }
+);
+
+const MainTabNavigator = createBottomTabNavigator(
+  {
+    [RouteName.PostTab]: PostStackNavigator,
+    [RouteName.AlbumTab]: AlbumStackNavigator
+  },
+  {
+    initialRouteName: "PostTab",
+    backBehavior: "none"
+  }
+);
+
+export default createAppContainer(MainTabNavigator);
