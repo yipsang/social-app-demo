03e8b9240e0ad53ca8d80f2cce0a2b0bed3dadee
Yip Sang Leung <thirtythousandollar@gmail.com>
2019-02-27 18:03:17 +0800

    Introduce apiClient

---
+++ src/apiClient.ts
@@ -0,0 +1,89 @@
+import * as Config from "./config";
+import { Post, postSchema } from "./models/Post";
+import { Comment, commentSchema } from "./models/Comment";
+import { Album, albumSchema } from "./models/Album";
+import { User, userSchema } from "./models/User";
+import { toQueryString } from "./utils/queryString";
+import { parseError } from "./error";
+
+export interface APIClient {
+    fetchPosts(cursor: number, limit: number): Promise<Post[]>;
+    fetchPostsOfUser(userId: number): Promise<Post[]>;
+    fetchComments(postId: number): Promise<Comment[]>;
+    fetchAlbums(cursor: number, limit: number): Promise<Album[]>;
+    fetchAlbumsOfUser(userId: number): Promise<Album[]>;
+    fetchUser(userId: number): Promise<User>;
+}
+
+async function _getAPI(
+    path: string,
+    params?: { [key: string]: any }
+): Promise<{ [key: string]: string }[]> {
+    let url = `${Config.API_ENDPOINT}/${path}`;
+    if (params) {
+        url += "?" + toQueryString(params);
+    }
+    return fetch(url).then(res => res.json());
+}
+
+export class SocialAppAPIClient implements APIClient {
+    async fetchPosts(cursor: number, limit: number): Promise<Post[]> {
+        try {
+            const res = await _getAPI("posts", {
+                _start: cursor,
+                _limit: limit
+            });
+            return Promise.all(res.map(r => postSchema.validate(r)));
+        } catch (e) {
+            throw parseError(e);
+        }
+    }
+
+    async fetchPostsOfUser(userId: number): Promise<Post[]> {
+        try {
+            const res = await _getAPI("posts", { userId });
+            return Promise.all(res.map(r => postSchema.validate(r)));
+        } catch (e) {
+            throw parseError(e);
+        }
+    }
+
+    async fetchComments(postId: number): Promise<Comment[]> {
+        try {
+            const res = await _getAPI("comments", { postId });
+            return Promise.all(res.map(r => commentSchema.validate(r)));
+        } catch (e) {
+            throw parseError(e);
+        }
+    }
+
+    async fetchAlbums(cursor: number, limit: number): Promise<Album[]> {
+        try {
+            const res = await _getAPI("albums", {
+                _start: cursor,
+                _limit: limit
+            });
+            return Promise.all(res.map(r => albumSchema.validate(r)));
+        } catch (e) {
+            throw parseError(e);
+        }
+    }
+
+    async fetchAlbumsOfUser(userId: number): Promise<Album[]> {
+        try {
+            const res = await _getAPI("albums", { userId });
+            return Promise.all(res.map(r => albumSchema.validate(r)));
+        } catch (e) {
+            throw parseError(e);
+        }
+    }
+
+    async fetchUser(userId: number): Promise<User> {
+        try {
+            const res = await _getAPI("users", { id: userId });
+            return userSchema.validate(res[0]);
+        } catch (e) {
+            throw parseError(e);
+        }
+    }
+}
