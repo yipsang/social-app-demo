9b5a01abb6233343e03fbdb6e819bc0d299c6b46
Yip Sang Leung <thirtythousandollar@gmail.com>
2019-03-02 13:14:43 +0800

    Add isLoading state in Paginator to prevent loading concurrently

--- src/paginator.ts
+++ src/paginator.ts
@@ -7,6 +7,7 @@ export interface PaginatorType<T> {
 }
 
 export class Paginator<T> implements PaginatorType<T> {
+    isLoading = false;
     isEnd = false;
     cursor = 0;
     pageSize = 0;
@@ -16,6 +17,7 @@ export class Paginator<T> implements PaginatorType<T> {
         paginatedAPICall: (cursor: number, limit: number) => Promise<T[]>,
         pageSize: number
     ) {
+        this.isLoading = false;
         this.isEnd = false;
         this.cursor = 0;
         this.pageSize = pageSize;
@@ -23,14 +25,16 @@ export class Paginator<T> implements PaginatorType<T> {
     }
 
     async next(): Promise<T[]> {
-        if (this.isEnd) {
+        if (this.isEnd || this.isLoading) {
             return Promise.resolve([]);
         }
+        this.isLoading = true;
         return this.paginatedAPICall(this.cursor, this.pageSize).then(data => {
             this.cursor += this.pageSize;
             if (data.length < this.pageSize) {
                 this.isEnd = true;
             }
+            this.isLoading = false;
             return data;
         });
     }
